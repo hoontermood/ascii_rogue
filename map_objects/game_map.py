86e640e7c8a6d60e7ea361ec15a7973d589697cb
hoontermood <yotedmoon@protonmail.com>
2019-10-02 17:53:02 -0700

    Implimented dungeon generation. SNAFU

--- map_objects/game_map.py
+++ map_objects/game_map.py
@@ -1,3 +1,6 @@
+from random import randint
+
+from map_objects.rectangle import Rect
 from map_objects.tile import Tile
 
 
@@ -9,16 +12,103 @@ class GameMap:
         self.tiles = self.initialize_tiles()
 
     def initialize_tiles(self):
-        tiles = [[Tile(False) for y in range(self.height)] for x in range(self.width)]
-
-        tiles[30][22].blocked = True
-        tiles[30][22].block_sight = True
-        tiles[31][22].blocked = True
-        tiles[31][22].block_sight = True
-        tiles[32][22].blocked = True
-        tiles[32][22].block_sight = True
+        # noinspection PyUnusedLocal
+        tiles = [[Tile(True) for y in range(self.height)] for x in range(self.width)]
 
         return tiles
+
+    def make_map(self, max_rooms, room_min_size, room_max_size, map_width, map_height, player):
+        """Generate the dungeon
+
+        :max_rooms: TODO
+        :room_min_size: TODO
+        :room_max_size: TODO
+        :map_width: TODO
+        :map_height: TODO
+        :player: TODO
+        :returns: TODO
+
+        """
+        rooms = []
+        num_rooms = 0
+
+        for r in range(max_rooms):
+            # random width and height
+            w = randint(room_min_size, room_max_size)
+            h = randint(room_min_size, room_max_size)
+            # random position within map bounds
+            x = randint(0, map_width - w - 1)
+            y = randint(0, map_height - h - 1)
+
+            new_room = Rect(x, y, w, h)
+
+            # Run through the other rooms checking for intersections
+            for other_room in rooms:
+                if new_room.intersect(other_room):
+                    break
+            else:
+                # Reaching this point means there are no intersections, room is valid
+                self.create_room(new_room)
+
+                # center coordinates of new room
+                (new_x, new_y) = new_room.center()
+
+                if num_rooms == 0:
+                    # starting room
+                    player.x = new_x
+                    player.y = new_y
+                else:
+                    # all rooms after the first:
+                    # connect to prev room via tunnel
+
+                    # center coordinates of prev room
+                    (prev_x, prev_y) = rooms[num_rooms - 1].center()
+
+                    # flip a coin
+                    if randint(0, 1) == 1:
+                        # move horizontally then vertically
+                        self.create_h_tunnel(prev_x, new_x, prev_y)
+                        self.create_v_tunnel(prev_y, new_y, new_x)
+                    else:
+                        # just the opposite
+                        self.create_v_tunnel(prev_y, new_y, prev_x)
+                        self.create_h_tunnel(prev_x, new_x, new_y)
+
+                # append new room to list
+                rooms.append(new_room)
+                num_rooms += 1
+
+    def create_room(self, room):
+        for x in range(room.x1 + 1, room.x2):
+            for y in range(room.y1 + 1, room.y2):
+                self.tiles[x][y].blocked = False
+                self.tiles[x][y].block_sight = False
+
+    def create_h_tunnel(self, x1, x2, y):
+        """Create a horizontal tunnel between rooms
+
+        :x1: TODO
+        :x2: TODO
+        :y: TODO
+        :returns: TODO
+
+        """
+        for x in range(min(x1, x2), max(x1, x2) + 1):
+            self.tiles[x][y].blocked = False
+            self.tiles[x][y].block_sight = False
+
+    def create_v_tunnel(self, y1, y2, x):
+        """Create a vertical tunnel between rooms
+
+        :y1: TODO
+        :y2: TODO
+        :x: TODO
+        :returns: TODO
+
+        """
+        for y in range(min(y1, y2), max(y1, y2) + 1):
+            self.tiles[x][y].blocked = False
+            self.tiles[x][y].block_sight = False
 
     def is_blocked(self, x, y):
         if self.tiles[x][y].blocked:
---
+++ map_objects/rectangle.py
@@ -0,0 +1,25 @@
+class Rect:
+    def __init__(self, x, y, w, h):
+        self.x1 = x
+        self.y1 = y
+        self.x2 = x + w
+        self.y2 = y + h
+
+    def center(self):
+        """Get center coordinates of rectangle
+        :returns: int
+
+        """
+        center_x = int((self.x1 + self.x2) / 2)
+        center_y = int((self.y1 + self.y2) / 2)
+        return (center_x, center_y)
+
+    def intersect(self, other):
+        """Returns True if given rect intersects with another
+
+        :other: Rect
+        :returns: boolean
+
+        """
+        return (self.x1 <= other.x2 and self.x2 >= other.x1 and
+                self.y1 <= other.y2 and self.y2 >= other.y1)
